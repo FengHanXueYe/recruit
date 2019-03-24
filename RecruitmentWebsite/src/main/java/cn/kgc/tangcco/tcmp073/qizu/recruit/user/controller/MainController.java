95b6bc9d544b31046ead9c8d25b6f35fe6d3dc12
Shinelon <Shinelon@DESKTOP-B85V28C>
2019-03-24 15:38:40 +0800

    包结构更改

--- RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/user/controller/MainController.java
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/user/controller/MainController.java
@@ -1,4 +1,4 @@
-package cn.kgc.tangcco.tcmp073.qizu.user.controller;
+package cn.kgc.tangcco.tcmp073.qizu.recruit.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
