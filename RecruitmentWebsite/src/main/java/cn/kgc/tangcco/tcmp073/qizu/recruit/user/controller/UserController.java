6e66ecaac51ee6144086d99c7d03f2646d437253
feng <505385234@qq.com>
2019-03-26 09:41:35 +0800

    usercontroller和userserviceimpl

---
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package cn.kgc.tangcco.tcmp073.qizu.recruit.user.controller;
+
+import org.springframework.stereotype.Controller;
+/**
+ * 用户servlet
+ * @author HASEE
+ *
+ */
+@Controller
+public class UserController {
+	
+	
+}
---
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package cn.kgc.tangcco.tcmp073.qizu.recruit.user.service.impl;
+
+import cn.kgc.tangcco.tcmp073.qizu.entity.RecruitingUsers;
+import cn.kgc.tangcco.tcmp073.qizu.recruit.user.service.UserService;
+
+/**
+ * 用户service实现类
+ * @author HASEE
+ *
+ */
+public class UserServiceImpl implements UserService {
+
+	
+	@Override
+	public int addUser(RecruitingUsers ruser) {
+		
+		return 0;
+	}
+
+	@Override
+	public int updateUser(RecruitingUsers ruser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteUser(Integer userid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public RecruitingUsers loginRecruitingUsers(RecruitingUsers ruser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
