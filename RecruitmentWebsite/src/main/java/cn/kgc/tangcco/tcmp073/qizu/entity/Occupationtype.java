b4bebd293c35026aa4e1843946de87980836529f
HP <HP@DESKTOP-AOEHUED>
2019-03-26 21:23:53 +0800

    增加实体类

---
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/entity/Occupationtype.java
@@ -0,0 +1,13 @@
+package cn.kgc.tangcco.tcmp073.qizu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Occupationtype {
+	private Integer id;
+	private String name;
+}
