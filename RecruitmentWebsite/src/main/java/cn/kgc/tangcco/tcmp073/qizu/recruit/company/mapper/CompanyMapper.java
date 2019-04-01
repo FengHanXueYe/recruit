5dff1e164b0fd850880f03fac70fc54295257983
Shinelon <Shinelon@DESKTOP-B85V28C>
2019-04-01 15:13:04 +0800

    123

--- RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/company/mapper/CompanyMapper.java
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/company/mapper/CompanyMapper.java
@@ -1,5 +1,25 @@
 package cn.kgc.tangcco.tcmp073.qizu.recruit.company.mapper;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import cn.kgc.tangcco.tcmp073.qizu.entity.Company;
+
 public interface CompanyMapper {
-			
+	@Insert("INSERT INTO recruitdb.company (cid,cuid,cphone, cemail ) VALUES(DEFAULT,#{cuid},#{cphone},#{cemail});")
+	int addCompanyOne(Company com);
+	@Select("select * from company where cemail=#{cemail}")
+	Company queryByEmail(String cemail);
+	@Update("update company set cname=#{cname} where cid=#{cid}")
+	int updateCompany(Company com);
+	@Update("update company set cabbreviation=#{cabbreviation},curl=#{curl},caddress=#{caddress},cfield=#{cfield},cfs=#{cfs},cdetail=#{cdetail},cscale=#{cscale} where cid=#{cid}")
+	int updateCompanyTwo(@Param("cabbreviation")String cabbreviation,@Param("curl")String curl,@Param("caddress")String caddress,@Param("cfield")String cfield,@Param("cfs")String cfs,@Param("cdetail")String cdetail,@Param("cscale")String cscale,@Param("cid")int cid);
+	@Update("update company set cfinancing=#{cfinancing} where cid=#{cid}")
+	int updateCompanyThree(@Param("cfinancing")String cfinancing,@Param("cid")int cid);
+	@Update("update company set companyProfile=#{companyProfile} where cid=#{cid}")
+	int updateCompanyProfile(@Param("companyProfile")String companyProfile,@Param("cid")int cid);
+
+	Company qyeryAllCompany(int cuid);
 }
