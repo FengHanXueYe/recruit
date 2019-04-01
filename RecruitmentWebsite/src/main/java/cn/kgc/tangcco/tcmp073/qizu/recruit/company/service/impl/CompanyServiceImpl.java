8ffc2d75f8d51961e4a98a502f65c29af7d2a300
Shinelon <Shinelon@DESKTOP-B85V28C>
2019-04-01 15:18:16 +0800

    123

---
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/company/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,46 @@
+package cn.kgc.tangcco.tcmp073.qizu.recruit.company.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+
+import cn.kgc.tangcco.tcmp073.qizu.entity.Company;
+import cn.kgc.tangcco.tcmp073.qizu.recruit.company.mapper.CompanyMapper;
+import cn.kgc.tangcco.tcmp073.qizu.recruit.company.service.CompanyService;
+@Controller
+public class CompanyServiceImpl implements CompanyService {
+	//123
+	@Resource
+	private CompanyMapper mapper;
+	@Override
+	public int addCompanyOne(Company com) {
+		return mapper.addCompanyOne(com);
+	}
+	@Override
+	public Company queryByEmail(String cemail) {
+		return mapper.queryByEmail(cemail);
+	}
+	@Override
+	public int updateCompany(Company com) {
+		return mapper.updateCompany(com);
+	}
+	@Override
+	public int updateCompanyTwo(String cabbreviation, String curl, String caddress, String cfield, String cfs,
+			String cdetail,String cscale,int cid) {
+		return mapper.updateCompanyTwo(cabbreviation, curl, caddress, cfield, cfs, cdetail,cscale,cid);
+	}
+	@Override
+	public int updateCompanyThree(String cfinancing, int cid) {
+		return mapper.updateCompanyThree(cfinancing, cid);
+	}
+	@Override
+	public int updateCompanyProfile(String companyProfile, int cid) {
+		return mapper.updateCompanyProfile(companyProfile, cid);
+	}
+	@Override
+	public Company qyeryAllCompany(int cuid) {
+		return mapper.qyeryAllCompany(cuid);
+	}
+	
+	//1232
+}
