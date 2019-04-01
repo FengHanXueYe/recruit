1544d45f3bbc5a0d71defb9dc07b7d4f85ba9b58
Shinelon <Shinelon@DESKTOP-B85V28C>
2019-04-01 15:17:06 +0800

    123

--- RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/company/service/CompanyService.java
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/company/service/CompanyService.java
@@ -1,5 +1,14 @@
 package cn.kgc.tangcco.tcmp073.qizu.recruit.company.service;
 
-public interface CompanyService {
+import cn.kgc.tangcco.tcmp073.qizu.entity.Company;
 
+public interface CompanyService {
+	int addCompanyOne(Company com);
+	Company queryByEmail(String cemail);
+	int updateCompany(Company com);
+	int updateCompanyTwo(String cabbreviation,String curl,String caddress,String cfield,String cfs,String cdetail,String cscale,int cid);
+	int updateCompanyThree(String cfinancing,int cid);
+	int updateCompanyProfile(String companyProfile,int cid);
+	Company qyeryAllCompany(int cuid);
+	//123
 }
