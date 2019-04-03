2319c15939d014266e116ed3dabc4b5679329ffb
HP <HP@DESKTOP-AOEHUED>
2019-04-03 17:12:22 +0800

    热门搜索

--- RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/occupation/service/OccupationService.java
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/occupation/service/OccupationService.java
@@ -1,7 +1,10 @@
 package cn.kgc.tangcco.tcmp073.qizu.recruit.occupation.service;
 
+import java.util.List;
+
 import cn.kgc.tangcco.tcmp073.qizu.entity.Occupation;
 
 public interface OccupationService {
 	int addOccupation(Occupation occ);
+	List<Occupation> queryall();
 }
