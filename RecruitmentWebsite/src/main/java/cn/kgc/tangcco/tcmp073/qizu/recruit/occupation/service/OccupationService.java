a01f4f6f077cc9267bfe511e83345fa2810d6684
feng <505385234@qq.com>
2019-04-07 09:33:56 +0800

    详情

--- RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/occupation/service/OccupationService.java
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/occupation/service/OccupationService.java
@@ -2,9 +2,13 @@ package cn.kgc.tangcco.tcmp073.qizu.recruit.occupation.service;
 
 import java.util.List;
 
+
 import cn.kgc.tangcco.tcmp073.qizu.entity.Occupation;
 
 public interface OccupationService {
 	int addOccupation(Occupation occ);
 	List<Occupation> queryall();//热门搜索
+	
+	Occupation queryOccupation(Integer oid);
+	
 }
