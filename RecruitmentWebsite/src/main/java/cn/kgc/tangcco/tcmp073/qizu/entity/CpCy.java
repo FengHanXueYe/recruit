6e970c058b26147ec50b90d1d274ae1646ab266a
feng <505385234@qq.com>
2019-04-07 11:22:31 +0800

    收藏公司VO

---
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/entity/CpCy.java
@@ -0,0 +1,19 @@
+package cn.kgc.tangcco.tcmp073.qizu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+/**
+ * 收藏和公司的VO
+ * @author HASEE
+ *
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CpCy {
+	
+	
+	private Collectionp collectionp;
+	private Company company;
+}
