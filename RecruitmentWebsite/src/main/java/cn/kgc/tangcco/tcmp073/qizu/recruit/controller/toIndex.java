06fade58dcbb2ec3110502fbdcdac694c5f45dcf
feng <505385234@qq.com>
2019-04-20 12:38:59 +0800

    aa

--- RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/controller/toIndex.java
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/recruit/controller/toIndex.java
@@ -1,7 +1,11 @@
 package cn.kgc.tangcco.tcmp073.qizu.recruit.controller;
 
 import org.springframework.stereotype.Controller;
-
+/**
+ * 测试提交
+ * @author HASEE
+ *
+ */
 @Controller
 public class toIndex {
 	
