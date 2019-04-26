cb767e9453676c02126dff0cbe648e75efbeb90c
HP <HP@DESKTOP-AOEHUED>
2019-04-26 16:02:50 +0800

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/FengHanXueYe/recruit.git

---
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/entity/ZuHe.java
@@ -0,0 +1,18 @@
+package cn.kgc.tangcco.tcmp073.qizu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ZuHe {
+	private Occupation occupation;
+	private String olog;
+	private String ename;
+	private int onature;
+	private int orelease;
+	private String oname;
+
+}
