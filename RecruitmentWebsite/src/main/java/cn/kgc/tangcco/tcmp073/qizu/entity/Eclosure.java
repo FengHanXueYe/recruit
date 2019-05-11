4c8dd366f712f5309eb3c6d497e227e9bafe14f2
feng <505385234@qq.com>
2019-05-11 11:27:09 +0800

    hah

---
+++ RecruitmentWebsite/src/main/java/cn/kgc/tangcco/tcmp073/qizu/entity/Eclosure.java
@@ -0,0 +1,16 @@
+package cn.kgc.tangcco.tcmp073.qizu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Eclosure {
+	
+	private Integer eid;
+	private String esurename;
+	private Integer euid;
+
+}
